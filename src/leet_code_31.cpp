80d0a821f37730d6f04b41371135892a09b302fd
Road__ <roadtlife0111925@163.com>
2024-10-16 00:04:44 +0800

    update leet_code_31 code add DLinkNode imp

--- src/leet_code_31.cpp
+++ src/leet_code_31.cpp
@@ -11,75 +11,169 @@
 
 #include <iostream>
 #include <unordered_map>
-#include <list>
+#include <vector>
+
+struct DLinkedNode
+{
+    int key;
+    int value;
+    DLinkedNode *pre;
+    DLinkedNode *next;
+
+    DLinkedNode(int x, int y) : key(x), value(y), pre(nullptr), next(nullptr) {}
+    ~DLinkedNode()
+    {
+        pre = nullptr;
+        next = nullptr;
+    }
+};
 
 class LRUCache
 {
-public:
-    // hash 表的value 存储的是堆外的position
-    std::unordered_map <int, std::list<std::pair<int, int>>::iterator> cacheMap;
-    // 存储的
-    std::list<std::pair<int, int>> cacheList;
+private:
     int capacity;
     int used;
+    DLinkedNode *head;
+    DLinkedNode *tail;
+    std::unordered_map<int, DLinkedNode*> hashTable;
 
-    // 以正整数作为容量 capacity 初始化 LRU 缓存
+public:
     LRUCache(int capacity)
     {
         this->capacity = capacity;
+        this->used = 0;
+        this->head = nullptr;
+        this->tail = nullptr;
     }
 
-    // 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1
     int get(int key)
     {
-        if (cacheMap.find(key) == cacheMap.end())
+        if (hashTable.find(key) != hashTable.end())
+        {
+            // 修改当前节点的指针
+            DLinkedNode *node = hashTable[key];
+
+            removeNode(node);
+            moveHead(node);
+            return node -> value;
+        }
+        else
         {
             return -1;
         }
-
-        std::list<std::pair<int, int>>::iterator it = cacheMap[key];
-        // 获取值
-        int value = it->second;
-        // 删除旧节点
-        cacheList.erase(it);
-        cacheList.push_front({key, value});
-        // 返回第一个元素的迭代器
-        cacheMap[key] = cacheList.begin();
-        return value;
     }
 
-    // 如果关键字已经存在，则变更其数据值；如果关键字不存在，则插入该组「关键字-值」。当缓存容量达到上限时，它应该在写入新数据之前删除最久未使用的数据值，从而为新的数据值留出空间。
     void put(int key, int value)
     {
-
-        if (cacheMap.find(key) == cacheMap.end())
+        DLinkedNode *node = nullptr;
+        if (hashTable.find(key) != hashTable.end())
         {
-            if (used >= capacity) {
-                // 获取尾部元素
-                int key = cacheList.back().first;
-                cachemap.erase(key);
-                cacheList.pop_back();
-            } else {
+            node = hashTable[key];
+            //更新值
+            node->key = key;
+            node->value = value;
+            //移除该节点
+            removeNode(node);
+
+            //移动头位置
+            moveHead(node);
+        }
+        else
+        {
+            if (used >= capacity)
+            {
+                // 删除hash表
+                DLinkedNode *node = hashTable[key];
+                hashTable.erase(key);
+                
+                removeNode(node);
+                // 更新新节点位置
+                moveHead(node);
+            }
+            else
+            {
+                node = new DLinkedNode(key, value);
+                if (tail == nullptr)
+                {
+                    tail = node;
+                }
+                else
+                {
+                    tail->next = node;
+                    node->pre = tail;
+
+                    if (head == nullptr)
+                    {
+                        head = node;
+                    }
+                    else
+                    {
+                        head->pre = node;
+                        node->next = head;
+                        head = node;
+                    }
+                }
                 used++;
             }
         }
-        else // 存在该节点
-        {
-            // 删除该节点
-            cacheList.erase(cacheMap[key]);
-        }
 
-        // 插入新节点
-        cacheList.push_front({key, value});
-        cacheMap[key] = cacheList.begin();
+        // 更新 key 的位置
+        hashTable[node->key] = head;
     }
 
+    void removeNode(DLinkedNode *node) {
+        node->pre->next = node -> next;
+        node->next->pre = node -> pre;
+        delete node;
+    }
+
+    void moveHead(DLinkedNode *node) {
+        //移动到头部
+        tail->next = node;
+        node->pre = tail;
+
+        node->next = head;
+        head->pre = node;
+
+        head = node;
+    }
 };
 
 int main()
 {
+    std::vector<std::string> input = {"LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"};
+    std::vector<std::vector<int>> params = {
+        {2},
+        {1, 1},
+        {2, 2},
+        {1},
+        {3, 3},
+        {2},
+        {4, 4},
+        {1},
+        {3},
+        {4}};
 
-    LRUCache *obj = new LRUCache(2);
+    LRUCache *obj = nullptr;
+    for (int i = 0; i < input.size(); ++i)
+    {
+        if (input[i] == "LRUCache")
+        {
+            obj = new LRUCache(params[i][0]);
+        }
+        else if (input[i] == "put")
+        {
+            obj->put(params[i][0], params[i][1]);
+        }
+        else if (input[i] == "get")
+        {
+            std::cout << obj->get(params[i][0]) << std::endl;
+        }
+        else
+        {
+            std::cout << "error" << std::endl;
+        }
+    }
 
     return 0;
 }
